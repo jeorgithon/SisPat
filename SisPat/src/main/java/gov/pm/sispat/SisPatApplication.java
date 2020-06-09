6f8cc67a32c0918276ab20b082cb1610793d7d8d
jeorgithon <jeor_d@hotmail.com>
2020-06-09 19:47:28 -0300

    Primeiro commit

---
+++ SisPat/src/main/java/gov/pm/sispat/SisPatApplication.java
@@ -0,0 +1,20 @@
+package gov.pm.sispat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SisPatApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(SisPatApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SisPatApplication.class, args);
+	}
+
+}
